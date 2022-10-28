4d8911447cba6cd9353be2351c70436ca38c621b
etienne-verraest <verraest.etienne@gmail.com>
2022-10-28 11:14:51 +0200

    Added RestController

---
+++ patients/src/main/java/com/abernathy/patients/controller/PatientController.java
@@ -0,0 +1,31 @@
+package com.abernathy.patients.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abernathy.patients.dao.PatientDao;
+import com.abernathy.patients.model.Patient;
+
+@RestController
+public class PatientController {
+
+	@Autowired
+	PatientDao patientDao;
+
+	/**
+	 * This method returns every patients registered in the clinic
+	 *
+	 * @return							ResponseEntity : List<Patient> containing every patient
+	 */
+	@GetMapping("/patients")
+	public ResponseEntity<List<Patient>> getPatients() {
+		List<Patient> patients = patientDao.getPatients();
+		return new ResponseEntity<>(patients, HttpStatus.OK);
+	}
+
+}
