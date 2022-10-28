197da277bba7cfe0503481853b354ded1d1fa966
etienne-verraest <verraest.etienne@gmail.com>
2022-10-28 11:04:15 +0200

    Added patient model

---
+++ patients/src/main/java/com/abernathy/patients/model/Patient.java
@@ -0,0 +1,40 @@
+package com.abernathy.patients.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "date_of_birth")
+	private Date dateOfBirth;
+
+	private char gender;
+
+	private String address;
+
+	private String phone;
+
+}
