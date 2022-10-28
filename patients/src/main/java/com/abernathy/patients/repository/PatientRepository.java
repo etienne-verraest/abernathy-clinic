9eb4ed94cefde42b97c615e51116f672e091c4f6
etienne-verraest <verraest.etienne@gmail.com>
2022-10-28 11:05:36 +0200

    Added Patient Repository

---
+++ patients/src/main/java/com/abernathy/patients/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.abernathy.patients.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abernathy.patients.model.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+}
