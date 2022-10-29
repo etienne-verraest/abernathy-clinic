9f85bf0fe41dfafaf1ffe2d9d20d8e3476155d20
etienne-verraest <verraest.etienne@gmail.com>
2022-10-29 16:24:18 +0200

    Differentiate Rest controller and Web controller

--- patients/src/main/java/com/abernathy/patients/controller/PatientRestController.java
+++ patients/src/main/java/com/abernathy/patients/controller/PatientRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,8 @@ import com.abernathy.patients.exceptions.PatientNotFoundException;
 import com.abernathy.patients.model.Patient;
 
 @RestController
-public class PatientController {
+@RequestMapping("api/")
+public class PatientRestController {
 
 	@Autowired
 	PatientDao patientDao;
