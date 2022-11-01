a302044323db8982a01de3719c6cc0d388cc57da
etienne-verraest <verraest.etienne@gmail.com>
2022-11-01 10:14:12 +0100

    Added Single Patient view

--- patients/src/main/java/com/abernathy/patients/controller/WebController.java
+++ patients/src/main/java/com/abernathy/patients/controller/WebController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.abernathy.patients.dao.PatientDao;
@@ -39,9 +40,9 @@ public class WebController {
 	 * If one patient is found, then we redirect to the patient view page.
 	 * Otherwise, we display search results with additionnal informations.
 	 *
-	 * @param searchPatientDto					SearchPatientDto containing filled fields
-	 * @return									Return different page based on the explanation
-	 * @throws PatientNotFoundException
+	 * @param searchPatientDto						SearchPatientDto containing filled fields
+	 * @return										Return different page based on the explanation
+	 * @throws PatientNotFoundException				Thrown if nobody was found
 	 */
 	@PostMapping("/search")
 	public String validateSearchPatientForm(@Valid SearchPatientDto searchPatientDto, BindingResult result, Model model)
@@ -72,5 +73,19 @@ public class WebController {
 			return "search";
 		}
 		return "index";
+	}
+
+	/**
+	 * Show the patient information with its ID
+	 *
+	 * @param id									String : the id of the patient
+	 * @return										PatientView.html
+	 * @throws PatientNotFoundException				Thrown if nobody was found
+	 */
+	@GetMapping("/search/{id}")
+	public String showPatientView(@PathVariable("id") String id, Model model) throws PatientNotFoundException {
+		Patient patient = patientDao.getPatientById(id);
+		model.addAttribute("patient", patient);
+		return "patientView";
 	}
 }
