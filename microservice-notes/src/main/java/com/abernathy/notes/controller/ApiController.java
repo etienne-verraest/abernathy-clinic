c7e00e045917f85ec3368cc0a09eeaa2173f9e4a
etienne-verraest <verraest.etienne@gmail.com>
2022-11-09 13:33:32 +0100

    Added POST Route

--- microservice-notes/src/main/java/com/abernathy/notes/controller/ApiController.java
+++ microservice-notes/src/main/java/com/abernathy/notes/controller/ApiController.java
@@ -7,11 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abernathy.notes.dao.NoteDao;
 import com.abernathy.notes.model.Note;
+import com.abernathy.notes.model.dto.NoteDto;
 
 @RestController
 @RequestMapping("api/")
@@ -21,15 +24,27 @@ public class ApiController {
 	NoteDao noteDao;
 
 	/**
-	 * Get notes for a given patient, based on its ID
+	 * Get a patient history, given its id
 	 *
 	 * @param id								String : The ID of the patient
 	 * @return									A List of notes concerning the patient
 	 */
 	@GetMapping("notes/{id}")
-	public ResponseEntity<List<Note>> getNotes(@PathVariable(required = true) String id) {
-
+	public ResponseEntity<List<Note>> getPatientHistory(@PathVariable(required = true) String id) {
 		List<Note> notes = noteDao.getNotesByPatientId(id);
 		return new ResponseEntity<>(notes, HttpStatus.OK);
+	}
+
+	/**
+	 * Add a note to the given patient
+	 *
+	 * @param noteDto							NoteDto : Object that contains attributes for the note
+	 * @return									Return the note if the operation is successful
+	 */
+	@PostMapping("notes")
+	public ResponseEntity<Note> addNoteToPatientHistory(@RequestBody NoteDto noteDto) {
+		Note note = noteDao.mapToEntity(noteDto);
+		noteDao.createNote(note);
+		return new ResponseEntity<>(note, HttpStatus.CREATED);
 	}
 }
