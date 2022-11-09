32898d9df998af6925bdbea508f43fa7af790be1
etienne-verraest <verraest.etienne@gmail.com>
2022-11-09 19:38:21 +0100

    Added more unit tests and implement Integration test on controllers

---
+++ microservice-notes/src/test/java/com/abernathy/notes/controller/ApiControllerTests.java
@@ -0,0 +1,117 @@
+package com.abernathy.notes.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.abernathy.notes.bean.PatientBean;
+import com.abernathy.notes.dao.NoteDao;
+import com.abernathy.notes.model.Note;
+import com.abernathy.notes.model.dto.NoteDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(controllers = ApiController.class)
+class ApiControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	NoteDao noteDaoMock;
+
+	static PatientBean patientMock;
+	static NoteDto noteDtoMock;
+	static Note noteMock;
+	static List<Note> notesListMock = new ArrayList<Note>();
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+
+		// Creating a mock patient for our operations
+		patientMock = new PatientBean();
+		patientMock.setFirstName("Alpha");
+		patientMock.setLastName("Bravo");
+		patientMock.setDateOfBirth("1998-02-16");
+		patientMock.setAddress("123 Imaginary Street");
+		patientMock.setGender("M");
+		patientMock.setPhone("123-400-5000");
+		patientMock.setId("AB10000");
+
+		String[] contents = { "He feels sleepy", "He feels sick" };
+		for (int i = 0; i < 2; i++) {
+			noteMock = new Note();
+			noteMock.setId("Note_" + i);
+			noteMock.setPatientId("AB10000");
+			noteMock.setPractitioner("Dr. Melrose");
+			noteMock.setContent(contents[i]);
+			notesListMock.add(noteMock);
+		}
+
+		noteDtoMock = new NoteDto();
+		noteDtoMock.setPractitioner("Dr. Melrose");
+		noteDtoMock.setPatientId("AB10000");
+		noteDtoMock.setContent("He feels sick");
+
+	}
+
+	@Test
+	void testGetPatientHistory_ShouldReturn_ListOfNotes() throws Exception {
+
+		// ARRANGE
+		when(noteDaoMock.getNotesByPatientId("AB10000")).thenReturn(notesListMock);
+
+		// ACT AND ASSERT
+		mockMvc.perform(get("/api/notes/{patientId}", "AB10000")) //
+				.andExpect(status().isOk()) //
+				.andExpect(jsonPath("$[0].id").value("Note_0")) //
+				.andExpect(jsonPath("$[1].id").value("Note_1"));
+	}
+
+	@Test
+	void testAddNoteToPatientHistory_ShouldBe_Successful() throws Exception {
+
+		// ARRANGE
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(noteDtoMock);
+
+		when(noteDaoMock.mapToEntity(any(NoteDto.class))).thenReturn(noteMock);
+		when(noteDaoMock.createNote(any(Note.class))).thenReturn(noteMock);
+
+		// ACT
+		mockMvc.perform(post("/api/notes").content(json).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$.patientId").value("AB10000"));
+
+	}
+
+	@Test
+	void testUpdateNoteById_ShouldBe_StatusOk() throws Exception {
+
+		// ARRANGE
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(noteDtoMock);
+		when(noteDaoMock.mapToEntity(any(NoteDto.class))).thenReturn(noteMock);
+		when(noteDaoMock.updateNote(anyString(), any(Note.class))).thenReturn(noteMock);
+
+		// ACT AND ASSERT
+		mockMvc.perform(put("/api/notes/{noteId}", "Note_1").content(json).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+
+	}
+
+}
--- microservice-notes/src/test/java/com/abernathy/notes/dao/NoteDaoTests.java
+++ microservice-notes/src/test/java/com/abernathy/notes/dao/NoteDaoTests.java
@@ -1,20 +1,28 @@
 package com.abernathy.notes.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import com.abernathy.notes.bean.PatientBean;
+import com.abernathy.notes.exception.NoteNotFoundException;
 import com.abernathy.notes.exception.PatientNotFoundException;
 import com.abernathy.notes.model.Note;
 import com.abernathy.notes.proxy.MicroservicePatientsProxy;
@@ -52,13 +60,14 @@ class NoteDaoTests {
 		patientMock.setId("AB10000");
 
 		noteMock = new Note();
+		noteMock.setId("Note_1");
 		noteMock.setPatientId("AB10000");
 		noteMock.setPractitioner("Dr. Melrose");
 		noteMock.setContent("He feels sick");
 	}
 
 	@Test
-	void testGetNotesByPatientId_ShouldReturn_1Note() throws PatientNotFoundException {
+	void testGetNotesByPatientId_ShouldReturn_ListOfNotes() throws PatientNotFoundException {
 
 		// ARRANGE
 		when(patientsProxyMock.getPatientById("AB10000")).thenReturn(patientMock);
@@ -73,7 +82,21 @@ class NoteDaoTests {
 	}
 
 	@Test
-	void testCreateNote_ShouldBe_Successful() throws PatientNotFoundException {
+	void testGetNotesByPatientId_ShouldThrowPatientNotFoundException() {
+
+		// ARRANGE
+		when(patientsProxyMock.getPatientById(anyString())).thenReturn(null);
+
+		// ACT
+		Executable executable = () -> noteDaoMock.getNotesByPatientId("XY30300");
+
+		// ASSERT
+		assertThrows(PatientNotFoundException.class, executable);
+		verify(noteRepositoryMock, never()).findByPatientId("XY30300");
+	}
+
+	@Test
+	void testCreateNote_ShouldBe_SuccessfulOperation() throws PatientNotFoundException {
 
 		// ARRANGE
 		when(patientsProxyMock.getPatientById("AB10000")).thenReturn(patientMock);
@@ -84,6 +107,51 @@ class NoteDaoTests {
 
 		// ASSERT
 		assertThat(response.getPractitioner()).isEqualTo("Dr. Melrose");
+	}
+
+	@Test
+	void testUpdateNote_ShouldBe_SuccessfulOperation() throws NoteNotFoundException, PatientNotFoundException {
+
+		// ARRANGE
+		when(noteRepositoryMock.findById(anyString())).thenReturn(Optional.of(noteMock));
+		when(patientsProxyMock.getPatientById("AB10000")).thenReturn(patientMock);
+		when(noteRepositoryMock.save(any(Note.class))).thenReturn(noteMock);
+
+		// ACT
+		Note response = noteDaoMock.updateNote("Note_1", noteMock);
+
+		// ASSERT
+		assertThat(response.getPractitioner()).isEqualTo("Dr. Melrose");
+		verify(noteRepositoryMock, times(1)).save(any(Note.class));
+	}
+
+	@Test
+	void testUpdateNote_ShouldThrow_NoteNotFoundException() {
+
+		// ARRANGE
+		when(noteRepositoryMock.findById(anyString())).thenReturn(Optional.empty());
+
+		// ACT
+		Executable executable = () -> noteDaoMock.updateNote("Note_30303030", noteMock);
+
+		// ASSERT
+		assertThrows(NoteNotFoundException.class, executable);
+
+	}
+
+	@Test
+	void testUpdateNote_ShouldThrow_PatientNotFoundException() {
+
+		// ARRANGE
+		when(noteRepositoryMock.findById(anyString())).thenReturn(Optional.of(noteMock));
+		when(patientsProxyMock.getPatientById(anyString())).thenReturn(null);
+
+		// ACT
+		Executable executable = () -> noteDaoMock.updateNote("Note_30303030", noteMock);
+
+		// ASSERT
+		assertThrows(PatientNotFoundException.class, executable);
+
 	}
 
 }
