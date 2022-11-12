45fcc483a7e60c082a6f1c3791886865de5e9ace
etienne-verraest <verraest.etienne@gmail.com>
2022-11-12 16:19:31 +0100

    Added Reports proxy on web-interface

---
+++ web-interface/src/main/java/com/abernathy/webinterface/proxy/MicroserviceReportsProxy.java
@@ -0,0 +1,14 @@
+package com.abernathy.webinterface.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.abernathy.webinterface.bean.ReportBean;
+
+@FeignClient(name = "microservice-reports", url = "localhost:9003")
+public interface MicroserviceReportsProxy {
+
+	@GetMapping("api/reports/generate/{patientId}")
+	ReportBean generateReport(@PathVariable String patientId);
+}
