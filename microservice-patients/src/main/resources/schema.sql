19f5c27e7cea247a52853a9eb62c6876049f8c3f
etienne-verraest <verraest.etienne@gmail.com>
2022-11-15 12:27:27 +0100

    Modified Database credential and schema

--- microservice-patients/src/main/resources/schema.sql
+++ microservice-patients/src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 --
 -- Table structure for table `patients`
 --
-
 DROP TABLE IF EXISTS `patients`;
 CREATE TABLE `patients` (
-  id int unsigned NOT NULL AUTO_INCREMENT,
+  id varchar(100) NOT NULL,
   first_name varchar(100) NOT NULL,
   last_name varchar(100) NOT NULL,
   date_of_birth date DEFAULT NULL,
   gender char(1) DEFAULT NULL,
   address varchar(255) DEFAULT NULL,
   phone varchar(150) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
