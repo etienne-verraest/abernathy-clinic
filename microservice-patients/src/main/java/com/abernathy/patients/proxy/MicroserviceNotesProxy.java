a45901a8be5b2f82f865fb3fe1624aafe526dd1f
etienne-verraest <verraest.etienne@gmail.com>
2022-11-15 11:31:47 +0100

    Added Notes microservice check when deleting patient

--- microservice-patients/src/main/java/com/abernathy/patients/proxy/MicroserviceNotesProxy.java
+++ microservice-patients/src/main/java/com/abernathy/patients/proxy/MicroserviceNotesProxy.java
@@ -1,12 +1,21 @@
 package com.abernathy.patients.proxy;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.abernathy.patients.bean.NoteBean;
 
 @FeignClient(name = "microservice-notes", url = "localhost:9002")
 public interface MicroserviceNotesProxy {
 
 	@DeleteMapping("api/notes/all/{patientId}")
 	boolean deleteAllNotesForPatientId(@PathVariable String patientId);
+
+	@GetMapping("api/notes/{patientId}")
+	List<NoteBean> getPatientHistory(@PathVariable String patientId);
+
 }
